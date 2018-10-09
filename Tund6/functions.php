b6f2e376169401de77d01a6c23be45018d8679a7
Roland Greebe <noormees123@users.noreply.github.com>
2018-10-09 11:48:12 +0300

    Lisatud lehtede vaheline liiklemine ja sõnumite valideerimine.

--- Tund6/functions.php
+++ Tund6/functions.php
@@ -1,73 +1,153 @@
 <?php
-	//laen andmebaasi info
+  //laen andmebaasi info
 	require("../../../config.php");
 	//echo $GLOBALS["serverUsername"];
 	$database = "if18_roland_gr_1";
 	
-	
-	//uue kasutaja lisamine
-	function signup($firstName, $lastName, $birthDate, $gender, $email ,$password){
-		$notice = "";
-		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"],$GLOBALS["database"]);
-		$stmt = $mysqli->prepare("INSERT INTO vpusers (firstname, lastname, birthdate, gender, email, password) VALUES (?, ?, ?, ?, ?, ?)");
-		echo $mysqli->error;
-		//valmistame parooli ette salvestamiseks - krüpteerime, teeme räsi (hash)
-		$options = ["cost" => 12, "salt" => substr(sha1(rand()), 0, 22)];
-		$pwdhash = password_hash($password, PASSWORD_BCRYPT, $options);
-		$stmt->bind_param("sssiss", $firstName, $lastName, $birthDate, $gender, $email ,$pwdhash);
-		if($stmt->execute()){
-			$notice = "Uue kasutaja lisamine õnnestus.";
+	//võtan kasutusele sessiooni
+	session_start();
+  
+  //sisselogimine
+  function signin($email, $password){
+	$notice = "";
+	$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+    $stmt = $mysqli->prepare("SELECT id, firstname, lastname, password FROM vpusers WHERE email=?");
+	$mysqli->error;
+	$stmt->bind_param("s", $email);
+	$stmt->bind_result($idFromDb, $firstnameFromDb, $lastnameFromDb, $passwordFromDb);
+	if($stmt->execute()){
+	  //kui õnnestus andmebaasist lugenine
+	  if($stmt->fetch()){
+		//leiti selline kasutaja
+	    if(password_verify($password, $passwordFromDb)){
+		  //parool õige
+		  $notice = "Logisite õnnelikult sisse!";
+		  $_SESSION["userId"] = $idFromDb;
+		  $_SESSION["firstName"] = $firstnameFromDb;
+		  $_SESSION["lastName"] = $lastnameFromDb;
+		  $stmt->close();
+	      $mysqli->close();
+		  header("Location: main.php");
+		  exit();
+		  
 		} else {
-			$notice = "Kasutaja lisamisel tekkis viga.". $stmt->error;
-		}
-		
-		
-		$stmt->close();
-		$mysqli->close();
-		return $notice;
+		  $notice = "Sisestasite vale salasõna!";
+        }
+	  } else {
+		$notice = "Sellist kasutajat (" .$email .") ei leitud!";  
+	  }		  
+	} else {
+	  $notice = "Sisselogimisel tekkis tehniline viga!" .$stmt->error;
 	}
-	
-	//anonüümse sõnumi salvestamine
-	function saveamsg($msg){
-		$notice = "";
-		//serveri ühendus (server, kasutaja, parool, andmbaas)
-		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"],$GLOBALS["database"]);
-		//valmistan ette sql käsu (statement)
-		$stmt = $mysqli->prepare("INSERT INTO vpamsg (message) VALUES(?)");
-		echo $mysqli->error;
-		//asendame sql käsus küsimärgi päris infoga (andmetüüp, andmed ise)
-		//s - string; i  - integer; d - decimal
-		$stmt->bind_param("s", $msg);
-		if ($stmt->execute()){
-			$notice = 'Sõnum: "'. $msg .'" on salvestatud.';
-		} else {
-			$notice = "Sõnumi salvestamisel tekkis tõrge: ". $stmt->error;
-		}
+	$stmt->close();
+	$mysqli->close();
+	return $notice;
+  }
+  
+  //kasutaja salvestamine
+  function signup($name, $surname, $email, $gender, $birthDate, $password){
+	$notice = "";
+	$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+	//kontrollime, ega kasutajat juba olemas pole
+	$stmt = $mysqli->prepare("SELECT id FROM vpusers WHERE email=?");
+	echo $mysqli->error;
+	$stmt->bind_param("s",$email);
+	$stmt->execute();
+	if($stmt->fetch()){
+		//leiti selline, seega ei saa uut salvestada
+		$notice = "Sellise kasutajatunnusega (" .$email .") kasutaja on juba olemas! Uut kasutajat ei salvestatud!";
+	} else {
 		$stmt->close();
-		$mysqli->close();
-		return $notice;
+		$stmt = $mysqli->prepare("INSERT INTO vpusers (firstname, lastname, birthdate, gender, email, password) VALUES(?,?,?,?,?,?)");
+    	echo $mysqli->error;
+	    $options = ["cost" => 12, "salt" => substr(sha1(rand()), 0, 22)];
+	    $pwdhash = password_hash($password, PASSWORD_BCRYPT, $options);
+	    $stmt->bind_param("sssiss", $name, $surname, $birthDate, $gender, $email, $pwdhash);
+	    if($stmt->execute()){
+		  $notice = "ok";
+	    } else {
+	      $notice = "error" .$stmt->error;	
+	    }
 	}
+	$stmt->close();
+	$mysqli->close();
+	return $notice;
+  }
+  //anonüümse sõnumi salvestamine
+  function saveamsg($msg){
+	$notice = "";
+	//serveri ühendus (server, kasutaja, parool, andmebaas
+	$mysqli = new mysqli($GLOBALS["serverHost"],$GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+	//valmistan ette SQL käsu
+	$stmt = $mysqli->prepare("INSERT INTO vpamsg (message) VALUES(?)");
+	echo $mysqli->error;
+	//asendame SQL käsus küsimargi päris infoga (andmetüüp, andmed ise)
+	//s - string; i - integer; d - decimal
+	$stmt->bind_param("s", $msg);
+	if ($stmt->execute()){
+	  $notice = 'Sõnum: "' .$msg .'" on salvestatud.';
+	} else {
+	  $notice = "Sõnumi salvestamisel tekkis tõrge: " .$stmt->error;
+	}
+	$stmt->close();
+	$mysqli->close();
+	return $notice;
+  }
+  
+  //valideerimata sõnumite lugemine
+  function readallunvalidatedmessages(){
+	$notice = "<ul> \n";
+	$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+	$stmt = $mysqli->prepare("SELECT id, message FROM vpamsg WHERE accepted IS NULL ORDER BY id DESC");
+	echo $mysqli->error;
+	$stmt->bind_result($id, $msg);
+	$stmt->execute();
 	
-	function listallmessages(){
+	while($stmt->fetch()){
+		$notice .= "<li>" .$msg .'<br><a href="validatemessage.php?id=' .$id .'">Valideeri</a>' ."</li> \n";
+	}
+	$notice.= "</ul> \n";
+	$stmt->close();
+	$mysqli->close();
+	return $notice;
+  }
+  
+  //loen sõnumi valideerimiseks
+  function readmsgforvalidation($editId){
+	$notice = "";
+	$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+	$stmt = $mysqli->prepare("SELECT message FROM vpamsg WHERE id = ?");
+	$stmt->bind_param("i", $editId);
+	$stmt->bind_result($msg);
+	$stmt->execute();
+	if($stmt->fetch()){
+		$notice = $msg;
+	}
+	$stmt->close();
+	$mysqli->close();
+	return $notice;
+  }
+  
+  function listallmessages(){
 	$msgHTML = "";
-	$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"],$GLOBALS["database"]);
-	$stmt = $mysqli->prepare("SELECT message FROM vpamsg");
+    $mysqli = new mysqli($GLOBALS["serverHost"],$GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+    $stmt = $mysqli->prepare("SELECT message FROM vpamsg1");
 	echo $mysqli->error;
 	$stmt->bind_result($msg);
 	$stmt->execute();
 	while($stmt->fetch()){
-		$msgHTML .="<p>" .$msg ."</p> \n";
+		$msgHTML .= "<p>" .$msg ."</p> \n";
 	}
 	$stmt->close();
 	$mysqli->close();
-	return $msgHTML;	
-	}
-	
-	
-	function test_input($data) {
-	$data = trim($data);
-	$data = stripslashes($data);
-	$data = htmlspecialchars($data);
-	return $data;
-	}
+	return $msgHTML;
+  }
+  
+  //tekstsisestuse kontroll
+  function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
 ?>
--- Tund6/newuser.php
+++ Tund6/newuser.php
@@ -1,109 +1,149 @@
 <?php
-	//Kutsume välja funktsioonide faili
-	require("functions.php");
-	
-	$notice = "";
-	
-	$firstName ="" ;
-	$lastName ="" ;
-	$birthMonth = null;
-	$birthDay = null;
-    $birthYear = null;
-	$birthDate = null;
-	$gender = null;
-	$email = "";
-	
-	$firstNameError = "";
-	$lastNameError = "";
-	$birthMonthError = "";
-	$birthDayError = "";
-    $birthYearError = "";
-	$birthDateError = "";
-	$genderError = "";
-	$emailError = "";
-	$passwordError = "";
-	
-	
-    $monthNow = date("m");
-	$monthNamesET = ["jaanuar", "veebruar", "märts", "aprill", "mai", "juuni","juuli", "august", "september", "oktoober", "november", "detsember"];
-	
-	//kontrollime kas kasutaja on nuppu vajutanud
-	if (isset($_POST["submitUserData"])){
-	//var_dump($_POST);
-		
-	if (isset($_POST["firstName"]) and !empty($_POST["firstName"])){
-	//$firstName = $_POST["firstName"];
-	$firstName = test_input($_POST["firstName"]);
+  require("functions.php");
+  $notice = "";
+  $firstName = "";
+  $lastName = "";
+  $email = "";
+  $gender = "";
+  $birthMonth = null;
+  $birthYear = null;
+  $birthDay = null;
+  $birthDate = null;
+  $monthNamesET = ["jaanuar", "veebruar", "märts", "aprill", "mai", "juuni","juuli", "august", "september", "oktoober", "november", "detsember"];
+  
+  //muutujad võimalike veateadetega
+  $nameError = "";
+  $surnameError = "";
+  $birthMonthError = "";
+  $birthYearError = "";
+  $birthDayError = "";
+  $birthDateError = "";
+  $genderError = "";
+  $emailError = "";
+  $passwordError = "";
+  $confirmpasswordError = "";
+  
+  //kui on uue kasutaja loomise nuppu vajutatud
+  if(isset($_POST["submitUserData"])){
+  
+  if (isset($_POST["name"]) and !empty($_POST["name"])){
+	$name = test_input($_POST["name"]);
+  } else {
+	  $nameError = "Palun sisesta eesnimi!";
+  }
+  
+  if (isset($_POST["surname"]) and !empty($_POST["surname"])){
+	$surname = test_input($_POST["surname"]);
+  } else {
+	  $surnameError = "Palun sisesta perekonnanimi!";
+  }
+  
+  if(isset($_POST["gender"])){
+	$gender = intval($_POST["gender"]);
+  } else {
+	  $genderError = "Palun märgi sugu!";
+  }
+  
+  //kontrollime, kas sünniaeg sisestati ja kas on korrektne
+  if(isset($_POST["birthDay"]) and !empty($_POST["birthDay"])){
+	  $birthDay = intval($_POST["birthDay"]);
+  } else {
+	  $birthDayError = "Palun vali sünnikuupäev!";
+  }
+  
+  if(isset($_POST["birthMonth"]) and !empty($_POST["birthMonth"])){
+	  $birthMonth = intval($_POST["birthMonth"]);
+  } else {
+	  $birthMonthError = "Palun vali sünnikuu!";
+  }
+  
+  if(isset($_POST["birthYear"]) and !empty($_POST["birthYear"])){
+	  $birthYear = intval($_POST["birthYear"]);
+  } else {
+	  $birthYearError = "Palun vali sünniaasta!";
+  }
+  
+  //kontrollin kuupäeva õigsust
+  if(!empty($birthDay) and !empty($birthMonth) and !empty($birthYear)){
+	//checkdate(päev, kuu, aasta)
+	if(checkdate($birthMonth, $birthDay, $birthYear)){
+	  $makebirthDate = date_create($birthMonth ."/" .$birthDay ."/" .$birthYear);
+	  $birthDate = date_format($makebirthDate, "Y-m-d");
+	  //echo $birthDate;
 	} else {
-		$firstNameError = "Palun sisestage oma eesnimi.";
-	}
-	
-	if (isset($_POST["lastName"])){
-	$lastName = test_input($_POST["lastName"]);
-	}
-	
-	if (isset($_POST["gender"]) and !empty($_POST["gender"])){
-		$gender = intval($_POST["gender"]);
-	} else {
-		$genderError = "Palun määra sugu.";
-	}
-	
-	//kui päev ja kuu ja aasta on olemas, kontrollitud
-	//võiks ju hoopis kontrollida, kas kuupäevadega seotud error muutujad on endiselt tühjad
-	if (isset($_POST["birthDay"]) and isset($_POST["birthMonth"]) and isset($_POST["birthYear"])){
-		//kas oodatav kuupäev on üldse võimalik
-		//checkdate(kuu, päev, aasta) tahab täisarve
-		if (checkdate(intval($_POST["birthMonth"]), intval($_POST["birthDay"]), intval($_POST["birthYear"]))){
-			//kui on võimalik, teeme kuupäevaks
-			$birthDate = date_create($_POST["birthMonth"] ."/". $_POST["birthDay"]. "/". $_POST["birthYear"]);
-			$birthDate = date_format($birthDate, "Y-m-d");
-			echo $birthDate;
-		} else {
-			$birthDateError = "Palun vali võimalik kuupäev.";
-		}
-	}
-	
-	//kui kõik on korras, siis salvestan kasutaja
-	
-	if(empty($firstNameError) and empty($lastNameError) and empty($birthMonthError) and empty($birthDayError) and empty($birthYearError) and empty($birthDateError) and empty($genderError) and empty($emailError) and empty($emailError)and empty($passwordError)){
-		$notice = signup($firstName, $lastName, $birthDate, $gender, $_POST["email"] ,$_POST["password"]);
-	}
-		
-	}//kas vajutati nuppu - lõpp
+	  $birthDateError = "Kuupäev on vigane!";
+    }
+  }//kui kõik kuupäeva osad on olmemas
+  
+  if (isset($_POST["email"]) and !empty($_POST["email"])){
+	//$name = $_POST["firstName"];
+	$email = test_input($_POST["email"]);
+  } else {
+	  $emailError = "Palun sisesta e-postiaadress!";
+  }
+  
+  if (!isset($_POST["password"]) or empty($_POST["password"])){
+	$passwordError = "Palun sisesta salasõna!";
+  } else {
+	  if(strlen($_POST["password"]) < 8){
+		  $passwordError = "Liiga lühike salasõna (sisestasite ainult " .strlen($_POST["password"]) ." märki).";
+	  }
+  }
+  
+  if (!isset($_POST["confirmpassword"]) or empty($_POST["confirmpassword"])){
+	$confirmpasswordError = "Palun sisestage salasõna kaks korda!";  
+  } else {
+	  if($_POST["confirmpassword"] != $_POST["password"]){
+		  $confirmpasswordError = "Sisestatud salasõnad ei olnud ühesugused!";
+	  }
+  }
+  
+  //kui kõik on korras, siis salvestame kasutaja
+  if(empty($nameError) and empty($surnameError) and empty($birthMonthError) and empty($birthYearError) and empty($birthDayError) and empty($genderError) and empty($emailError) and empty($passwordError) and empty($confirmpasswordError)){
+    $notice = signup($name, $surname, $email, $gender, $birthDate, $_POST["password"]);
+  }
+  
+  }//kui on nuppu vajutatud - lõppeb
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-  <meta charset="utf-8">
-  <title>Poiss õpib</title>
-</head>
-<body>
-  <h1>Roland Greebe, IF18</h1>
-  <p>See leht on loodud <a href="http://www.tlu.ee" target="_blank">TLÜ</a> õppetöö raames, ei pruugi parim välja näha ning kindlasti ei sisalda tõsiseltvõetavat sisu!</p>
-  
-  <hr>
-    
-  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-  <label>Eesnimi:</label></br>
-  <input type="text" name="firstName" value="<?php echo $firstName; ?>"><span><?php echo $firstNameError; ?></span></br>
-  <label>Perekonnanimi:</label></br>
-  <input type="text" name="lastName"><span><?php echo $lastNameError; ?></span></br>
-  <label>Sünnipäev: </label>
-	  <?php
-	    echo '<select name="birthDay">' ."\n";
-		for ($i = 1; $i < 32; $i ++){
-			echo '<option value="' .$i .'"';
-			if ($i == $birthDay){
-				echo " selected ";
+  <head>
+    <meta charset="utf-8">
+	<title>Katselise veebi uue kasutaja loomine</title>
+  </head>
+  <body>
+    <h1>Loo endale kasutajakonto</h1>
+	<p>See leht on valminud <a href="http://www.tlu.ee" target="_blank">TLÜ</a> õppetöö raames ja ei oma mingisugust, mõtestatud või muul moel väärtuslikku sisu.</p>
+	<hr>
+	
+	<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+	  <label>Eesnimi:</label><br>
+	  <input name="name" type="text" value="<?php echo $name; ?>"><span><?php echo $nameError; ?></span><br>
+      <label>Perekonnanimi:</label><br>
+	  <input name="surname" type="text" value="<?php echo $surname; ?>"><span><?php echo $surnameError; ?></span><br>
+	  
+	  <input type="radio" name="gender" value="2" <?php if($gender == "2"){		echo " checked";} ?>><label>Naine</label>
+	  <input type="radio" name="gender" value="1" <?php if($gender == "1"){		echo " checked";} ?>><label>Mees</label><br>
+	  <span><?php echo $genderError; ?></span><br>
+	  
+	  <label>Sünnipäev: </label>
+		  <?php
+			echo '<select name="birthDay">' ."\n";
+			echo '<option value="" selected disabled>päev</option>' ."\n";
+			for ($i = 1; $i < 32; $i ++){
+				echo '<option value="' .$i .'"';
+				if ($i == $birthDay){
+					echo " selected ";
+				}
+				echo ">" .$i ."</option> \n";
 			}
-			echo ">" .$i ."</option> \n";
-		}
-		echo "</select> \n";
-	  ?>
-	   <label>Sünnikuu: </label>
+			echo "</select> \n";
+		  ?>
+	  <label>Sünnikuu: </label>
 	  <?php
 	    echo '<select name="birthMonth">' ."\n";
+		echo '<option value="" selected disabled>kuu</option>' ."\n";
 		for ($i = 1; $i < 13; $i ++){
 			echo '<option value="' .$i .'"';
 			if ($i == $birthMonth){
@@ -113,11 +153,11 @@
 		}
 		echo "</select> \n";
 	  ?>
-	   <label>Sünniaasta: </label>
-	  <!--<input name="birthYear" type="number" min="1914" max="2003" value="1998">-->
+	  <label>Sünniaasta: </label>
 	  <?php
 	    echo '<select name="birthYear">' ."\n";
-		for ($i = date("Y"); $i >= date("Y") - 100; $i --){
+		echo '<option value="" selected disabled>aasta</option>' ."\n";
+		for ($i = date("Y") - 15; $i >= date("Y") - 100; $i --){
 			echo '<option value="' .$i .'"';
 			if ($i == $birthYear){
 				echo " selected ";
@@ -126,26 +166,20 @@
 		}
 		echo "</select> \n";
 	  ?>
-	  </br>
+	  <br>
+	  <span><?php echo $birthDateError ." " .$birthDayError ." " .$birthMonthError ." " .$birthYearError; ?></span>
+	  <br>
 	  
-	  <input type="radio" name="gender" value="2" <?php if($gender == 2){ echo "checked";} ?>><label>Naine</label></br>
-	  <input type="radio" name="gender" value="1" <?php if ($gender == 1){ echo "checked";} ?>><label>Mees</label></br>
-	  <span><?php echo $genderError; ?></span>
-	  
-	  
-	  <label>E-postiaadress (kasutajatunnuseks)</label></br>
-	  <input name="email" type="email">
-	  </br>
-	  
-	  <label>Salasõna (min 8 märki):</label></br>
-	  <input name="password" type="password"></br>
-	  
-	  <input type="submit" name="submitUserData" value="Saada andmed">
-	  
-	  </form>
-  <hr>
-  <p><?php echo $notice; ?></p>
-  
-  
-</body>
+	  <label>E-mail (kasutajatunnus):</label><br>
+	  <input type="email" name="email" value="<?php echo $email; ?>"><span><?php echo $emailError; ?></span><br>
+	  <label>Salasõna (min 8 tähemärki):</label><br>
+	  <input name="password" type="password"><span><?php echo $passwordError; ?></span><br>
+	  <label>Korrake salasõna:</label><br>
+	  <input name="confirmpassword" type="password"><span><?php echo $confirmpasswordError; ?></span><br>
+	  <input name="submitUserData" type="submit" value="Loo kasutaja"><span><?php echo $notice; ?></span>
+	</form>
+	<hr>
+	<p><a href="index_3.php">Tagasi</a> avalehele!</p>
+	
+  </body>
 </html>
